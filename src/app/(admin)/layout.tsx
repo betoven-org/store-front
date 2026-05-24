e8d516158013dca998bb66b796c4df55b54f618d
Wesley Moraes <wesleymoraesserafim@gmail.com>
2026-05-24 20:46:07 -0300

    feat: novo favicon com logo "b" do Brasa CMS

--- src/app/(admin)/layout.tsx
+++ src/app/(admin)/layout.tsx
@@ -24,6 +24,7 @@ export default function AdminRootLayout({
   return (
     <html lang="pt-BR" className={`${geist.variable} ${geistMono.variable} antialiased`}>
       <head>
+        <link rel="icon" href="/favicon.svg" type="image/svg+xml" />
         <link rel="icon" href="/favicon.ico" sizes="any" />
         <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
         <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
