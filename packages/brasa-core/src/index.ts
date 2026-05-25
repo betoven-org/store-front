c0d966e38dcc8cad95b0cb48de20629c5899214a
Wesley Moraes <wesleymoraesserafim@gmail.com>
2026-05-25 08:06:34 -0300

    fix: atualizar exports do brasa-core (remover handlers/signIn/signOut do NextAuth)

--- packages/brasa-core/src/index.ts
+++ packages/brasa-core/src/index.ts
@@ -1,6 +1,6 @@
 export * from "./schema";
 export { db } from "./db";
-export { auth, handlers, signIn, signOut } from "./auth";
+export { auth, neonAuth } from "./auth";
 export * from "./search";
 export * from "./validations";
 export * from "./slug";
